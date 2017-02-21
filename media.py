df2e70ba43fdf78c84b79cb56e0d91e7cb348490
Oscar Barillas <oscarbarillas93@gmail.com>
2017-02-20 21:47:50 -0800

    Modified comments on media.py

--- media.py
+++ media.py
@@ -1,16 +1,18 @@
 import webbrowser
-
 class Movie():
     """
     The Movies class contains the title, storyline, poster_image_url, and trailer_youtube_url attributes.
 
-    The show_trailer function opens the corresponding 
+    The show_trailer function opens the browser
     """
+
+    # _init_ initializes the attributes for Movies. Self referes to the newly created object.
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    # The show trailer function opens the youtube url to a selected Movie instance.
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
