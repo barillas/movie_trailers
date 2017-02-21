04b5a21fae37685ab3835c9d5b481c132215859d
Oscar Barillas <oscarbarillas93@gmail.com>
2017-02-20 21:43:03 -0800

    Initial Commit

---
+++ media.py
@@ -0,0 +1,16 @@
+import webbrowser
+
+class Movie():
+    """
+    The Movies class contains the title, storyline, poster_image_url, and trailer_youtube_url attributes.
+
+    The show_trailer function opens the corresponding 
+    """
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Movie Trailer Website - Oscar Barillas
+
+## Introduction
+
+This program generates a Movie Trailer website so users can see my favorite Movies with an ability to see the trailers in a modal.
+
+This program was written in Python. The latest version of Python2 or Python3 is recommended. 
+
+## Installation
+
+In order to run this program, use the command:
+'python command_center.py' in your terminal.
